06c050832d05155cdccbc438a91f705913f7fd4d
d3z41k <d3z41k@gmail.com>
2017-02-20 18:59:01 +0300

    stage42

--- ss-scripts-2017-1/dev-reg.js
+++ ss-scripts-2017-1/dev-reg.js
@@ -3,32 +3,6 @@
 const config = require('config');
 const _ = require('lodash/array');
 
-async function getMargin(contractSum, params) {
-  return new Promise(async (resolve, reject) => {
-
-    let margin = 0;
-    let sub = 0;
-
-    // = Add to sub P (debt) =
-    sub += Number(params[1]);
-
-    // = Add to sub salaryData and other cost =
-    for (let i = 2; i < params.length; i++) {
-      params[i].forEach((value) => {
-        sub += Number(value);
-      });
-    }
-
-    for (let c = 0; c < contractSum.length; c++) {
-      if(contractSum[c][0] == params[0]) {
-        margin = contractSum[c][1] - sub;
-      }
-    }
-
-    resolve(margin);
-  });
-
-}
 
 //--------------------------------------------------------------------------
 // Main function
@@ -53,6 +27,7 @@ async function devReg() {
     const devRegQuery = require('../models-2017-1/db_dev-reg-query');
     const getPlanHours = require('../libs/dev-reg/getPlanHours');
     const getRatioHours = require('../libs/dev-reg/getRatioHours');
+    const getMargin = require('../libs/dev-reg/getMargin');
     let abc = require('../libs/abc')();
 
     async function start(auth) {
